f7d5881fd5a57e212ab314e66a277850fa97899b
Pavan Uthsara <uthsarapavan@gmail.com>
2024-06-13 08:29:57 +0530

    update configure.php - version 1

--- configure.php
+++ configure.php
@@ -1,3 +1,13 @@
 <?php
 //How do we add a common db?
+    $host = "localhost";
+    $username = "host";
+    $password = "";
+    $dbname = "car_rental_db";
+
+    $con = new mysqli ($host, $username, $password, $dbname);
+
+    if($con->connect_error){
+        die ("Connection error : " . $con->connect_error);
+    }
 ?>
